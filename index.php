ef2c5c236bbba10f215710cbdb6d6a4d9c0c94c5
Marcelo Ferreira <marceloffdeoliveira@gmail.com>
2018-04-16 17:34:30 -0300

    reescrita layout

--- index.php
+++ index.php
@@ -1,94 +1,95 @@
-<?php session_start();?>
 <!DOCTYPE html>
   <html>
     <head>
       <!--Import Google Icon Font-->
       <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-      <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
-      
       <!--Import materialize.css-->
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
-      <link rel="stylesheet" href="css/style.css">
 
       <!--Let browser know website is optimized for mobile-->
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-     
-      <title>Página Principal</title>
+
     </head>
 
-    <body>
-
-      <!-- MENU USUARIO -->
-
-      <section class="usuario z-depth-2">
-        <div class="header-usuario">
-          <div class="container">
-            <div class="row">
-              <div class="col l12">
-                <div class="avatar-usuario">
-                  <img class="responsive-img circle z-depth-2" src="imgs/avatar/foto.jpg">
+    <body class="grey lighten-5">
+      <header>
+      <ul id="slide-out" class="sidenav sidenav-fixed">
+        <li>
+          <div class="header-usuario">
+              <div class="container">
+                <div class="row">
+                  <div class="col l12">
+                    <div class="avatar-usuario">
+                      <img class="responsive-img circle z-depth-2" src="foto.jpg">
+                    </div>
+                  </div>
                 </div>
-              </div>
+                <div class="row">
+                  <div class="col l12">
+                    <div class="info-usuario">
+                      <div class="userName">Marcelo Ferreira</div>
+                      <div class="nickName">(MarceloMustaine)</div>
+                    </div>
+                  </div>
+                </div>
             </div>
-            <div class="row">
-              <div class="col l12">
-                <div class="info-usuario">
-                  <div class="userName">Marcelo Ferreira</div>
-                  <div class="nickName">(MarceloMustaine)</div>
-                </div>
-              </div>
+        </li>
+          <li><a href="#!">Home</a></li>
+          <li>
+            <ul class="collapsible">
+              <li>
+                <a class="collapsible-header">Adicionar</a>
+                    <div class="collapsible-body">
+                      <ul>
+                        <li><a href="#">Adicionar Projeto</a></li>
+                        <li><a href="#">Adicionar Volume</a></li>
+                        <li><a href="#">Adicionar Capítulo</a></li>
+                      </ul>
+                  </div>
+              </li>
+              <li>
+                <a class="collapsible-header">Modificar</a>
+                    <div class="collapsible-body">
+                      <ul>
+                        <li><a href="#">Modificar Projeto</a></li>
+                        <li><a href="#">Modificar Volume</a></li>
+                        <li><a href="#">Modificar Capítulo</a></li>
+                      </ul>
+                  </div>
+              </li>
+              <li>
+                <a class="collapsible-header">Excluir </a>
+                    <div class="collapsible-body">
+                      <ul>
+                        <li><a href="#">Excluir Projeto</a></li>
+                        <li><a href="#">Excluir Volume</a></li>
+                        <li><a href="#">Excluir Capítulo</a></li>
+                      </ul>
+                  </div>
+              </li>
+            </ul>
+          </li>
+      </ul>
+      <a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+      </header>
+      <main>
+        <div class="row">
+          <div class="col s11">
+            <div class="card welcome">
+              <h4>Bem vindo, <b>Marcelo!</b></h4>
             </div>
           </div>
-          <div class="menu">
-          <ul class="collapsible sidenav sidenav-fixed">
-            <li class="active"><a class="collapsible-header" href="/sistema">Home</a></li>
-            <li>
-              <a class="collapsible-header">Projeto <i class="material-icons">arrow_drop_down</i></a>
-              <div class="collapsible-body">
-                <ul>
-                  <li><a href="#">Adicionar Projeto</a></li>
-                  <li><a href="#">Adicionar Volume</a></li>
-                  <li><a href="#">Adicionar Capítulo</a></li>
-                  <li><a href="#">Modificar Projeto</a></li>
-                  <li><a href="#">Modificar Volume</a></li>
-                  <li><a href="#">Modificar Capítulo</a></li>
-                  <li><a href="#">Excluir Projeto</a></li>
-                  <li><a href="#">Excluir Volume</a></li>
-                  <li><a href="#">Excluir Capítulo</a></li>
-                </ul>
-              </div>
-            </li>
-          </ul>
-          </div>
+          <div class="separador"></div>
         </div>
-      </section>
-
-
-
-
-
-
-
-
-      <!-- FIM MENU USUARIO -->
-
-		<?php /*
-
-      if($_SESSION['success']){
-			 echo @$_SESSION['success'];
-		  }
-*/
-		?>
-
-
+      </main>
+      <!--JavaScript at end of body for optimized loading-->
       <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
       <script type="text/javascript">
-        var elem = document.querySelector('.sidenav');
-        var instance = M.Sidenav.init(elem, 'options');
-      </script>
-      <script type="text/javascript">
         var elem = document.querySelector('.collapsible');
-        var instance = M.Collapsible.init(elem, 'options');
+      var instance = M.Collapsible.init(elem, 'options');
+
+      var elem = document.querySelector('.sidenav');
+      var instance = M.Sidenav.init(elem, 'options');
       </script>
     </body>
   </html>
